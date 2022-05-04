dee7c99a594f8579e7b2f7bbddda8ba85f851f4e
Kwandes <bz.jasiek@gmail.com>
2022-05-04 21:49:01 +0200

    feat: 🎸 Add a LOG table entity model thing

--- apps/api/src/app/config/config.service.ts
+++ apps/api/src/app/config/config.service.ts
@@ -1,4 +1,4 @@
-import { Name, NameTag, Shitpost, ShitpostTag, User } from '@models';
+import { Log, Name, NameTag, Shitpost, ShitpostTag, User } from '@models';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -41,7 +41,7 @@ class ConfigService {
       password: this.getValue('MYSQL_PASSWORD', false) || 'root',
       database: this.getValue('MYSQL_DATABASE', false) || 'shitpost_generator',
 
-      entities: [User, Shitpost, ShitpostTag, Name, NameTag],
+      entities: [User, Shitpost, ShitpostTag, Name, NameTag, Log],
     };
   }
 }
